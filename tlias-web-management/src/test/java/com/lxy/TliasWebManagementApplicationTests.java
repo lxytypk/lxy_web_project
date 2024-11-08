0a2682f0e210a7044b64262c0a47de32060a97f0
lxy <a1242753955@163.com>
2024-11-08 22:20:15 +0800

    hot-fix commit01

--- tlias-web-management/src/test/java/com/lxy/TliasWebManagementApplicationTests.java
+++ tlias-web-management/src/test/java/com/lxy/TliasWebManagementApplicationTests.java
@@ -64,7 +64,7 @@ class TliasWebManagementApplicationTests {
         //根据bean的名称 及 类型获取
         DeptController bean3 = applicationContext.getBean("deptController", DeptController.class);
         System.out.println(bean3);
-        System.out.println("test git!");
+        System.out.println("test git lxy!");
     }
 
 }
